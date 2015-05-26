4b6ffd88a7d5cdce3a6f45a1a5fddce72d416e70
Carlos Cesar Barajas Jr <soldotsol@gmail.com>
2015-05-25 20:37:56 -0700

    Restructure to match express generator

--- app.js
+++ app.js
@@ -1,29 +1,61 @@
-/* Load packages */
 require('dotenv').load();
 var express = require('express');
-//var mongodb = require('mongodb').MongoClient;
-//var mongoose = require('mongoose');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-var appTitle = 'Shoe Runner';
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
 var app = express();
-var apiRouter = express.Router();
 
-//setup express settings
-app.set('views', './views');
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use('/api', apiRouter);
-app.use(express.static('public'));//grants public access to all items below this directory
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-/* Router paths */
+app.use('/', routes);
+app.use('/users', users);
 
-/* App paths */
-app.get('/', function (req, res){
-  res.render('index', {title: appTitle});
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-var server = app.listen(process.env.APP_PORT, function(){
-  console.log('listening on port %s', process.env.APP_PORT);
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
+
 
 module.exports = app;
